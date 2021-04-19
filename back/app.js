e9f739fae0a490425ee2693aa6428b8ddabbab9a
metasmash <oualiyannick@hotmail.fr>
2021-04-19 22:12:46 +0200

    fuck we cannot get R script processing and response.

--- back/app.js
+++ back/app.js
@@ -1,1 +1,10 @@
 const app = require('./server')
+const R = require('r-script')
+
+app.get('/hw', async (req, res) => {
+    console.log('someone entered...')
+    var out = R('ex-sync.R').data('hello world', 20).callSync()
+    console.log(out)
+
+    res.send('hello world')
+})
--- back/server.js
+++ back/server.js
@@ -4,9 +4,7 @@ const cors = require('cors')
 const app = express()
 
 const corsOptions = {
-    origin: [
-        'http://localhost:3000/',
-    ],
+    origin: ['http://localhost:3000'],
 }
 
 app.use(cors(corsOptions))
@@ -23,12 +21,12 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json())
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`)
 })
 
 module.exports = app
