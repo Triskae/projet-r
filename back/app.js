1ff2ee9ca727dfb802838e3efafd0ce3ef458035
metasmash <oualiyannick@hotmail.fr>
2021-04-20 15:27:13 +0200

    one extra example

--- back/app.js
+++ back/app.js
@@ -1,11 +1,24 @@
 const app = require('./server')
 const R = require('r-script')
 
+const core = (rScript) => `../core/${rScript}`
+
 // this is a hello world to connect R to nodejs.
 app.get('/hw', async (req, res) => {
     console.log('R is processing.')
-    R('../core/ex-sync.R')
+    R(core('ex-sync.R'))
         .data('hello world', 20)
+        .call((err, out) => {
+            if (err) throw err
+            res.send(out)
+        })
+    console.log('R has processed.')
+})
+
+app.get('/test', async (req, res) => {
+    console.log('R is processing.')
+    R(core('test.R'))
+        .data({ toto: 'The output string' })
         .call((err, out) => {
             if (err) throw err
             res.send(out)
