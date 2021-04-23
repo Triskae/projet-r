8a7bc62c125865514821b123d7779c69b23aae03
metasmash <oualiyannick@hotmail.fr>
2021-04-23 19:52:27 +0200

    types update

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 const R = require('./Rscripts')
 const app = require('./server')
 
+//for decision tree
 app.get('/classifier/rpart', async (req, res) => {
     const inputs = {
         arg1: req.body.arg1, // "gini" || "information"
@@ -15,16 +16,51 @@ app.get('/classifier/rpart', async (req, res) => {
 })
 
 //for random forest
-app.get('/classifier/rf', async () => {})
+app.get('/classifier/rf', async (req, res) => {
+    const inputs = {
+        arg1: req.body.arg1, // number ex: 300, 500, ...
+        arg2: req.body.arg2, // number ex: 3, 5, ...
+        arg3: req.body.arg3, // boolean
+        arg4: req.body.arg4, // color ex: 'orange', 'green', 'red', 'blue', ...
+    }
+})
 
 //k nearest neighbors
-app.get('/classifier/knn', async () => {})
+app.get('/classifier/knn', async (req, res) => {
+    const inputs = {
+        arg1: req.body.arg1, // number ex: 10, 20, ...
+        arg2: req.body.arg2, // number ex: 5, 10, ...
+        arg3: req.body.arg3, // boolean
+        arg4: req.body.arg4, // color ex: 'orange', 'green', 'red', 'blue', ...
+    }
+})
 
 //support vector machine
-app.get('/classifier/svm', async () => {})
+app.get('/classifier/svm', async (req, res) => {
+    const inputs = {
+        arg1: req.body.arg1, // "gini" || "information"
+        arg2: req.body.arg2, // number ex: 5, 10, ...
+        arg3: req.body.arg3, // boolean
+        arg4: req.body.arg4, // color ex: 'orange', 'green', 'red', 'blue', ...
+    }
+})
 
 //naive bayes
-app.get('/classifier/nb', async () => {})
+app.get('/classifier/nb', async (req, res) => {
+    const inputs = {
+        arg1: req.body.arg1, // "gini" || "information"
+        arg2: req.body.arg2, // number ex: 5, 10, ...
+        arg3: req.body.arg3, // boolean
+        arg4: req.body.arg4, // color ex: 'orange', 'green', 'red', 'blue', ...
+    }
+})
 
 //neural network
-app.get('/classifier/nn', async () => {})
+app.get('/classifier/nn', async (req, res) => {
+    const inputs = {
+        arg1: req.body.arg1, // "gini" || "information"
+        arg2: req.body.arg2, // number ex: 5, 10, ...
+        arg3: req.body.arg3, // boolean
+        arg4: req.body.arg4, // color ex: 'orange', 'green', 'red', 'blue', ...
+    }
+})
