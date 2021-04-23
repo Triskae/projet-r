f25681c8a4ae25b184f55b2f540718856767ec74
metasmash <oualiyannick@hotmail.fr>
2021-04-23 19:55:14 +0200

    fix helpers

--- back/Rscripts/helpers.js
+++ back/Rscripts/helpers.js
@@ -1,6 +1,5 @@
 const _ = require('lodash')
 
-
 module.exports = {
-  Rlist_to_array: (Rlist) => [_.keys(Rlist[0]), ..._.map(Rlist, _.values)];
+    Rlist_to_array: (Rlist) => [_.keys(Rlist[0]), ..._.map(Rlist, _.values)],
 }
