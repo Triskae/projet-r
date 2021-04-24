9cc6afede34ea0bc6c79906ff9c579b705f7e2ec
Filipe <filipe.doutelsilva@hotmail.fr>
2021-04-24 14:16:53 +0200

    Added image

--- back/helpers.js
+++ back/helpers.js
@@ -1,19 +1,27 @@
 const _ = require('lodash')
 const csvPath = './data/dataset.csv'
 const csv = require('csvtojson')
+const fs = require('fs');
 
 const getDataset = async () => await csv().fromFile(csvPath)
 
 const Rlist_to_array = (Rlist) => [_.keys(Rlist[0]), ..._.map(Rlist, _.values)]
 
 const rScripts = {
-    decisionTree: 'rpart.R',
-    randomForest: 'rforest.R',
-    kNearestNeighbors: 'kknn.R',
-    supportVectorMachine: 'svm.R',
+    decisionTree: 'rpart',
+    randomForest: 'rforest',
+    kNearestNeighbors: 'kknn',
+    supportVectorMachine: 'svm',
+}
+
+const base64Encode = (fileName) => {
+    const bitmap = fs.readFileSync('./images/' + fileName + '.jpg');
+    console.log(new Buffer(bitmap).toString('base64'));
+    return new Buffer(bitmap).toString('base64');
 }
 
 module.exports = {
+    base64Encode,
     getDataset,
     Rlist_to_array,
     rScripts,
